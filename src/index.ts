7fcd2231ca3353dbb5007af61c757dfa7d1d3f55
Luis <luis@computer.org>
2017-03-29 00:11:09 -0600

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 export * from './services/login.service';
